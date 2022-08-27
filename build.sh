535222cc6e9427dbe68f3a0f992510d88844228b
saiprashanthts <saiprashanthts@gmail.com>
2022-08-27 16:27:04 -0400

    added build.sh

---
+++ build.sh
@@ -0,0 +1,28 @@
+# !bin/sh
+declare -a PYTHON_PACKAGES=("github_archive")
+
+rm - rf build
+echo "creating the folder for downloading the python packages present in requirements.txt"
+mkdir build
+
+
+rm - rf build_code
+echo "creating the folder for lambda code"
+mkdir build_code
+
+
+python3 -m pip install requirements.txt -t build
+
+for package in ${PYTHON_PACKAGES[@]}
+do
+  echo "Zipping the contents present inside "$package
+  zip -rq build_code/$package package
+  cd build
+  echo "Adding the packages present in requirements.txt to the package "$package
+  zip -rq ../build_code/$package .
+done
+
+echo "Process Completed"
+
+cd ..
+rm -rf build
