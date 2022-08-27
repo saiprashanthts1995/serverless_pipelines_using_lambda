ddd270e3e103a558be30fff55d60ce7d126979bd
saiprashanthts <saiprashanthts@gmail.com>
2022-08-27 14:31:53 -0400

    updated

--- github_archive/__init__.py
+++ github_archive/__init__.py
@@ -3,6 +3,11 @@ import os
 # sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 from github_archive.services.s3_service import S3Service
 
+def lambda_handler(event, context):
+    s3 = S3Service()
+    print(s3.s3_list_buckets())
+    print(os.path.join(os.path.dirname(__file__)),"..")
+
 
 if __name__ == "__main__":
     s3 = S3Service()
