f9d2d375cb91a54ef1dc7e2724599e531e3b396d
saiprashanthts <saiprashanthts@gmail.com>
2022-09-03 22:36:15 -0400

    added

--- github_archive/__init__.py
+++ github_archive/__init__.py
@@ -8,6 +8,10 @@ def lambda_handler(event, context):
     s3 = S3Service()
     print(s3.s3_list_buckets())
     print(os.path.join(os.path.dirname(__file__)), "..")
+    # call bookmark module to find the latest file
+    # call create_file_path to get the list of files
+    # call download file using the above list
+    # update the bookmark table with the last extracted file
 
 
 if __name__ == "__main__":
